715307adff7a7b8b77663b77e6d93200cb85cdc2
chhaya <schhaya1997@gmail.com>
2022-01-15 12:42:37 +0530

    day two catalog app

---
+++ lib/pages/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      drawer: Drawer(backgroundColor: Colors.blueAccent),
+      body:Center(
+         child: Container(
+            child:Text("hello i am text here"),
+         ),
+      ),
+    );
+  }
+
+} 
