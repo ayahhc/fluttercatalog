da71f01df8815242000ae3b7652755981fece702
chhaya <schhaya1997@gmail.com>
2022-01-28 19:50:48 +0530

    bad code to rescue and stble code of add to cart and remove from cart

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -3,15 +3,16 @@ import 'package:catalog_app/models/catalog.dart';
 import 'package:catalog_app/widgets/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
+
 class CartPage extends StatelessWidget {
-  const CartPage({ Key? key }) : super(key: key);
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      backgroundColor: MyTheme.creamColor,
-      title: "Cart".text.make(),
+        backgroundColor: MyTheme.creamColor,
+        title: "Cart".text.make(),
       ),
       body: Column(
         children: [
@@ -25,7 +26,7 @@ class CartPage extends StatelessWidget {
 }
 
 class _CartTotal extends StatelessWidget {
-  const _CartTotal({ Key? key }) : super(key: key);
+  const _CartTotal({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,19 +38,19 @@ class _CartTotal extends StatelessWidget {
         children: [
           "\$${_cart.TotalPrice}".text.xl5.amber500.make(),
           30.widthBox,
-          ElevatedButton(onPressed: (){
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-               content:"Buying not supported yet".text.make(),
-            ),
-            );
-          }, 
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(MyTheme.bluishColor)
-            ),
-          
-          child: "BUY".text.white.xl.bold.make()
-          ).w32(context),
-     
+          ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: "Buying not supported yet".text.make(),
+                      ),
+                    );
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(MyTheme.bluishColor)),
+                  child: "BUY".text.white.xl.bold.make())
+              .w32(context),
         ],
       ),
     );
@@ -57,27 +58,30 @@ class _CartTotal extends StatelessWidget {
 }
 
 class _CartList extends StatefulWidget {
-  const _CartList({ Key? key }) : super(key: key);
+  const _CartList({Key? key}) : super(key: key);
 
   @override
   State<_CartList> createState() => _CartListState();
 }
 
 class _CartListState extends State<_CartList> {
-
-final _cart = CartModel();
+  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _cart.items.length,
-      itemBuilder: (context,index)=>ListTile(
-        leading: Icon(Icons.done),
-        trailing: IconButton(onPressed: (){}, 
-           icon: Icon(Icons.remove_circle_outline)
-           ),
-           title:_cart.items[index].name.text.make(),
-      ),
-      
-    );
+    return _cart.items.isEmpty
+        ? "Nothing to Show".text.xl3.makeCentered()
+        : ListView.builder(
+            itemCount: _cart.items.length,
+            itemBuilder: (context, index) => ListTile(
+              leading: Icon(Icons.done),
+              trailing: IconButton(
+                  onPressed: () {
+                    _cart.remove(_cart.items[index]);
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.remove_circle_outline)),
+              title: _cart.items[index].name.text.make(),
+            ),
+          );
   }
 }
---
+++ lib/widgets/home_widget/add_to_cart.dart
@@ -0,0 +1,44 @@
+
+import 'package:catalog_app/models/cart_page.dart';
+import 'package:catalog_app/models/catalog.dart';
+import 'package:catalog_app/widgets/theme.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class AddtoCart extends StatefulWidget {
+  final Item? catalog;
+
+  const AddtoCart({Key? key,  this.catalog}) : super(key: key);
+  // const AddtoCart({ Key? key }) : super(key: key);
+
+  @override
+  State<AddtoCart> createState() => AddtoCartState();
+}
+
+class AddtoCartState extends State<AddtoCart> {
+   final _cart = CartModel();
+  @override
+  Widget build(BuildContext context) {
+bool IsinCart = _cart.items.contains(widget.catalog);
+    return  Column(
+      children: [
+        ElevatedButton(onPressed: (){
+          if(!IsinCart){
+          IsinCart = IsinCart.toggle();
+          final _catalog = CatalogModel();
+          final _cart = CartModel();
+           _cart.catalog = _catalog;
+          _cart.add(widget.catalog!);
+          setState(() {});
+        }
+        },
+                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(MyTheme.bluishColor),
+                      shape: MaterialStateProperty.all(StadiumBorder(),
+                      )),
+                        child: IsinCart?Icon(Icons.done): Icon(CupertinoIcons.cart_badge_plus)
+                        ),
+      ],
+    );
+  }
+}
